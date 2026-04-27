4ca1319a1c6fe09370ea8eba9897df649f38e709
Milind D <milind@Milinds-MacBook-Air.local>
2026-04-27 18:01:57 +0530

    Spring

---
+++ Springfrm3/src/cs/res/Empres.java
@@ -0,0 +1,12 @@
+package cs.res;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("cs.bean")
+public class Empres {
+	
+	
+
+}
